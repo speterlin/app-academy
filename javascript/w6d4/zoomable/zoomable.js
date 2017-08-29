4a7b48a2e95d64262a2cc36cae1fbc031c5b138b
Sebastian Peterlin <speterlin@gmail.com>
2017-08-29 11:21:56 +0200

    Clean and ensure javascript/w6d4/zoomable/zoomable.js works correctly, add images to javascript/w6d4/zoomable/ diretory for further testing, ensure these images are ignored by git by modifying .gitignore.

--- javascript/w6d4/zoomable/zoomable.js
+++ javascript/w6d4/zoomable/zoomable.js
@@ -3,14 +3,15 @@ $.Zoomable = function (target, options) {
   this.$target.addClass('zoomable');
   this.boxSize = ((options && options.boxSize) || 100);
   this.$target.on('mousemove', this.showFocusBox.bind(this));
-  // this.$target.on('mouseleave', this.removeFocusBox.bind(this));
+  this.$target.on('mouseleave', this.removeFocusBox.bind(this));
 }
 
 $.Zoomable.prototype.showFocusBox = function (event) {
   if (!this.mousedOver) {
     this.mousedOver = true;
     this.$focusBox = $('<div class="focus-box"></div>');
-    this.$focusBox.css('height', this.boxSize).css('width', this.boxSize);
+    this.$focusBox.css('height', this.boxSize).css('width', this.boxSize)
+      //  if want to make box a circle add: .css('border-radius', '50%');
     this.$target.append(this.$focusBox);
 
     this.offsetY = this.$target.offset().top;
@@ -19,26 +20,16 @@ $.Zoomable.prototype.showFocusBox = function (event) {
     var img = this.$target.find('img');
     this.imgWidth = img.innerWidth();
     this.imgHeight = img.innerHeight();
-    console.log("img dimensions (x,y):"+this.imgWidth, this.imgHeight);
   }
 
   var xDiff = event.pageX - this.offsetX - (this.boxSize / 2);
   var yDiff = event.pageY - this.offsetY - (this.boxSize / 2);
-  console.log("diffs (x,y)"+xDiff, yDiff);
 
-  if (xDiff < 0) {
-    xDiff = 0;
-  }
-  if (yDiff < 0) {
-    yDiff = 0;
-  }
-
-  if (xDiff > this.imgWidth - this.boxSize / 2) {
-    xDiff = this.imgWidth - this.boxSize / 2;
-  }
-  if (yDiff > this.imgHeight - this.boxSize / 2) {
-    yDiff = this.imgHeight - this.boxSize / 2;
-  }
+  // maybe refactor, hack job to ensure box doesn't leave boundary of image
+  xDiff = xDiff < 0 ? 0 : xDiff;
+  xDiff = xDiff > (this.imgWidth - this.boxSize) ? (this.imgWidth - this.boxSize) : xDiff;
+  yDiff = yDiff < 0 ? 0 : yDiff;
+  yDiff = yDiff > (this.imgHeight - this.boxSize) ? (this.imgHeight - this.boxSize) : yDiff;
 
   this.$focusBox.css('left', xDiff).css('top', yDiff);
   this.showZoom(xDiff, yDiff);
@@ -49,38 +40,31 @@ $.Zoomable.prototype.removeFocusBox = function(event) {
   this.$focusBox.remove();
 
   this.zoomed = false;
-  // this.$zoom.remove();
 };
 
 $.Zoomable.prototype.showZoom = function (xDiff, yDiff) {
   if (!this.zoomed) {
     this.zoomed = true;
-    this.windowHeight = window.innerHeight;
-    console.log(this.windowHeight, this.imgWidth);
 
-    //% of Image size
-    this.blowUpScale = (this.imgWidth * 2 / this.boxSize) * 100;
-    console.log('blowUpScale: ' + this.blowUpScale);
-    // this.$zoom = $('<div class="zoomed-image"></div>');
-    // this.$zoom
-    //   .css('background-image', 'url('+this.$target.find('img').attr('src')+')')
-    //   .css('width', this.windowHeight)
-    //   .css('background-size', this.blowUpScale + '% auto')
+    //assuming image is square, works with rectangular images with minor errors at edges
+    this.blowUpScale = this.imgWidth / this.boxSize;
+
+    this.zoomRatio = 2;
+
+    this.finalRatio = this.blowUpScale * this.zoomRatio;
+
+    //needs to be refatored, hack job to ensure zoom covers entire photo by ensuring that the furthest corner point is included
+    this.factorX = ((this.zoomRatio * this.imgWidth) - this.boxSize) / ((this.imgWidth - this.boxSize) * this.zoomRatio)
+    this.factorY = ((this.zoomRatio * this.imgHeight) - this.boxSize) / ((this.imgHeight - this.boxSize) * this.zoomRatio)
+
     this.$focusBox
         .css('background-image', 'url('+this.$target.find('img').attr('src')+')')
-        .css('background-size', this.blowUpScale + '% auto')
-
-    $('body').append(this.$zoom);
+        .css('background-size', (this.finalRatio * 100) + '% auto')
   }
-  console.log(this.blowUpScale, xDiff, this.imgWidth);
-  //calculations assume that image is square
-  xDiff = ((this.blowUpScale * xDiff) / this.imgWidth);
-  yDiff = ((this.blowUpScale * yDiff) / this.imgWidth);
-  console.log('new diffs(x,y)', xDiff, yDiff)
-  //img sized compared to window size
-  // var ratio = 2;
-  // this.$zoom.css('background-position', '-'+ xDiff + 'px -' + yDiff + 'px');
-  this.$focusBox.css('background-position', '-'+ xDiff + 'px -' + yDiff + 'px');
+  var newX = xDiff * this.zoomRatio * this.factorX;
+  var newY = yDiff * this.zoomRatio * this.factorY;
+
+  this.$focusBox.css('background-position', '-'+ newX + 'px -' + newY + 'px');
 }
 
 $.fn.zoomable = function (options) {
